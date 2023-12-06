3b960e3dbf15992fa299da6b1860766aca781297
OUAFIDI <o.ouafidi@shellboxes.com>
2023-12-06 20:24:57 +0100

    Creating an errorHandling Middleware [filesUpdated: middleware=>error.js, routes=>(apartment.js, superAdmin.js, syndic.js), server.js]

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,15 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const errorHandler = require('./middleware/error')
 
 app.use(express.json())
 app.use(cors())
+app.use(errorHandler)
+
 
 const indexRoute = require('./routes')
-app.use('/api', indexRoute)
+// app.use('/api', indexRoute)
 
 app.listen(process.env.PORT, ()=>{
     console.log(`App is started on ${process.env.PORT} port`);
